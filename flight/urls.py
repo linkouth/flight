dc4e235e99c505e2e59de09ad30134a5fcac1883
linkouth <dkatimullin@gmail.com>
2019-03-04 08:42:30 +0300

    add proc3 flight/views.py

--- flight/urls.py
+++ flight/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.procedure_list, name='procedure_list'),
     path('proc1', views.proc1, name='proc1'),
+    path('proc2', views.proc2, name='proc2'),
+    # path('proc3', views.proc3, name='proc3'),
 ]
--- flight/views.py
+++ flight/views.py
@@ -15,3 +15,25 @@ def proc1(request):
         'param': param,
         'rows': rows
     })
+
+
+def proc2(request):
+    param = request.POST['param']
+    with connection.cursor() as cursor:
+        cursor.execute("declare @seats_count int; exec proc2 %s, @seats_count output; select @seats_count;", [param])
+        rows = cursor.fetchall()
+    return render(request, 'flight/proc1.html', {
+        'param': param,
+        'rows': rows,
+    })
+
+
+# def proc3(request):
+#     param = request.POST['param']
+#     with connection.cursor() as cursor:
+#         cursor.execute("declare @a int; exec @a = proc4 %s; if (@a = 1) return 't' else return 'f'", [param])
+#         rows = cursor.fetchall()
+#     return render(request, 'flight/proc1.html', {
+#         'param': param,
+#         'rows': rows,
+#     })
